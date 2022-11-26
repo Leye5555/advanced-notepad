cfbccfadeb5f6c6e97c3afa1ea25059d1912ac17
Olubummo Faith <aaeon456engineer@gmail.com>
2022-11-26 15:48:10 +0100

    feature: added form component with tag support

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { Form, Stack, Row, Col } from 'react-bootstrap';
+import CreateReactSelect from 'react-select/creatable';
 
 const NoteForm = () => {
-  return <div>NoteForm</div>;
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreateReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Stack>
+    </Form>
+  );
 };
 
 export default NoteForm;
